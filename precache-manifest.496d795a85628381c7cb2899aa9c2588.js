56af92d1fed115138d656e08259b4becc2e3f9d3
birddev <birddev@gmail.com>
2021-04-20 02:53:59 -0700

    test

---
+++ precache-manifest.496d795a85628381c7cb2899aa9c2588.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1bd6fe8f9c8ae7613ed",
+    "url": "/static/css/main.a0ddc5fe.chunk.css"
+  },
+  {
+    "revision": "a1bd6fe8f9c8ae7613ed",
+    "url": "/static/js/main.a1bd6fe8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ab212e82c9bf8c58c248",
+    "url": "/static/css/2.0e190378.chunk.css"
+  },
+  {
+    "revision": "ab212e82c9bf8c58c248",
+    "url": "/static/js/2.ab212e82.chunk.js"
+  },
+  {
+    "revision": "e25638942204214e9eb7e94ddf79fc69",
+    "url": "/static/media/audit.e2563894.pdf"
+  },
+  {
+    "revision": "bd5f17673db766cd71dfd4c82cf8b17a",
+    "url": "/static/media/bird.bd5f1767.png"
+  },
+  {
+    "revision": "afa08cd95ca924af2bfb30d0d67ec6b1",
+    "url": "/static/media/eth.afa08cd9.png"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "/static/media/mdx.8e72f6f2.png"
+  },
+  {
+    "revision": "35d75be1f670537add991c4d07ee2602",
+    "url": "/static/media/eggBg.35d75be1.png"
+  },
+  {
+    "revision": "cb4308ea3570cb5148313a625dff0545",
+    "url": "/static/media/birds.cb4308ea.png"
+  },
+  {
+    "revision": "f7a0b62f80d069f4e509d34b73c71e87",
+    "url": "/index.html"
+  }
+];
